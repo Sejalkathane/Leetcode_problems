fd06fbfd85f592bac2c11ea3a47f1f37cf8c0c77
Sejal kathane <kathanesejal@gmail.com>
2022-10-15 19:46:10 +0530

    problem no 1

---
+++ Data_Structure_14_day_challenge/day2_1_Two_Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+//     unordered_map<int, int> mp;
+//     for (int i = 0; i < nums.size(); ++i) {
+//       if (mp.count(target - nums[i])) {
+//         return {mp[target - nums[i]], i};
+//       }
+//       tmp[nums[i]] = i;
+//     }
+//     return {-1, -1};
+      
+//     }
+        
+        
+        vector<int>v;
+        
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<nums.size();++i){
+            if(mp.find(target-nums[i])!=mp.end()){
+                v.push_back(i);
+                v.push_back(mp[target-nums[i]]);
+                return v;
+            }
+            mp[nums[i]]=i;
+        }
+            
+     return v;
+    }
+            
+};
