13ead957c2cf9cc430842a22ba569b28e2bf45ed
Sejal kathane <kathanesejal@gmail.com>
2022-10-15 19:43:42 +0530

    problem no 53

---
+++ Data_Structure_14_day_challenge/day1_53_Maximum Subarray.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int n=nums.size();
+//         int sum=0,res=INT_MIN;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             sum+=nums[i];
+//             res=max(sum,res);
+//             if(sum < 0) 
+//                 sum = 0;
+//         }
+//         return res;
+        
+        
+        
+        
+        
+         int res=nums[0];
+        int mxend=nums[0];
+        
+        for(int i=1;i<n;i++)
+      {
+          mxend=max(mxend+nums[i],nums[i]);
+          res=max(res,mxend);
+      }
+        return res;
+    }
+        
+    
+};
