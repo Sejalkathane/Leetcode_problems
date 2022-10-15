92170ab64de208414556cf7a1b7425734d1334ff
Sejal kathane <kathanesejal@gmail.com>
2022-10-15 19:50:29 +0530

    problem no 121

--- Data_Structure_14_day_challenge/day3_350_Intersection_of_two_ArrayII.cpp
+++ Data_Structure_14_day_challenge/day3_350_Intersection_of_two_ArrayII.cpp
@@ -1,40 +1,40 @@
-class Solution {
-public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
-{
-//          unordered_set<int> s;
-//         int m=nums1.size();
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
+// {
+// //          unordered_set<int> s;
+// //         int m=nums1.size();
 
-//    for (int i = 0; i < m; i++)
-//    {
-//       s.insert(nums1[i]);
-//    }
+// //    for (int i = 0; i < m; i++)
+// //    {
+// //       s.insert(nums1[i]);
+// //    }
 
-//      // int res = 0;
-//      int n=nums2.size();
-//    for (int i = 0; i < n; i++)
-//    {
-//       if (s.find(nums2[i]) != s.end())
-//       {
-//          cout<<nums2[i]<<" ";
-//          s.erase(nums2[i]);
-//       }
-//    }
+// //      // int res = 0;
+// //      int n=nums2.size();
+// //    for (int i = 0; i < n; i++)
+// //    {
+// //       if (s.find(nums2[i]) != s.end())
+// //       {
+// //          cout<<nums2[i]<<" ";
+// //          s.erase(nums2[i]);
+// //       }
+// //    }
         
-         unordered_map<int, int> freq;
-        vector<int> res;
+//          unordered_map<int, int> freq;
+//         vector<int> res;
         
-        for (auto num : nums1) freq[num]++;
+//         for (auto num : nums1) freq[num]++;
         
-        for (auto num : nums2) {
-            if (freq[num]) {
-                res.push_back(num);
-                freq[num]--;
-            }
-        }
+//         for (auto num : nums2) {
+//             if (freq[num]) {
+//                 res.push_back(num);
+//                 freq[num]--;
+//             }
+//         }
         
-        return res;
-    }
+//         return res;
+//     }
 
     
-};
+// };
