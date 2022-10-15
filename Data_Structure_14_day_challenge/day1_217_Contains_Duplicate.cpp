35117c3c277fd034ad33157acd54ec1c5e6e0863
Sejal kathane <kathanesejal@gmail.com>
2022-10-15 19:38:50 +0530

    Problem no 217

---
+++ Data_Structure_14_day_challenge/day1_217_Contains_Duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+		sort(nums.begin(), nums.end());
+		for(int i = 1; i < nums.size(); i++)
+		{
+			if(nums[i-1] == nums[i])
+				return true;
+		}
+		return false;
+        
+        // if set size is not equal to that then we say that it contain duplicate
+        //        unordered_set<int> s(nums.begin(), nums.end());
+        // return (s.size() != nums.size());
+	}
+};
