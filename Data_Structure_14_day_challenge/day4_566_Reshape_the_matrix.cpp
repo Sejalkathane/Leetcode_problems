5d7c1ba513f4b9a0131575381cfc1032dca41aed
Sejal kathane <kathanesejal@gmail.com>
2022-10-16 22:42:52 +0530

    problem no 118 day 4

--- Data_Structure_14_day_challenge/day4_566_Reshape_the_matrix.cpp
+++ Data_Structure_14_day_challenge/day4_566_Reshape_the_matrix.cpp
@@ -1,19 +1,19 @@
-class Solution {
-public:
-    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+// class Solution {
+// public:
+//     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         
-      int m = size(mat), n = size(mat[0]), total = m * n;
+//       int m = size(mat), n = size(mat[0]), total = m * n;
         
-        if(r * c != total) return mat;
+//         if(r * c != total) return mat;
         
-        vector<vector<int>> ans(r, vector<int>(c));
+//         vector<vector<int>> ans(r, vector<int>(c));
         
-        for(int i = 0; i < total; i++) 
-            ans[i / c][i % c] = mat[i / n][i % n];
+//         for(int i = 0; i < total; i++) 
+//             ans[i / c][i % c] = mat[i / n][i % n];
         
-        return ans;
+//         return ans;
       
         
         
-    }
-};
+//     }
+// };
