6d1f7063357121c16f4e5cf390aa22fe5ab85e09
Sejal kathane <kathanesejal@gmail.com>
2022-10-20 18:44:46 +0530

    problem no 141

--- Data_Structure_14_day_challenge/day6_242_valid_anagram.cpp
+++ Data_Structure_14_day_challenge/day6_242_valid_anagram.cpp
@@ -1,30 +1,30 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-//         sort(s.begin(),s.end());
-//         sort(t.begin(),t.end());
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+// //         sort(s.begin(),s.end());
+// //         sort(t.begin(),t.end());
         
-//         if(s==t)
-//              return true;
-//         else
-//             return false;
+// //         if(s==t)
+// //              return true;
+// //         else
+// //             return false;
         
         
         
-       if (s==t) return true;
+//        if (s==t) return true;
         
-        if (s.size()!=t.size()) return false;
+//         if (s.size()!=t.size()) return false;
         
-        unordered_map <char, int> umap;
-        for (int i=0; i<s.size(); i++) {
-            umap[s[i]]++;
-            umap[t[i]]--;
-        }
+//         unordered_map <char, int> umap;
+//         for (int i=0; i<s.size(); i++) {
+//             umap[s[i]]++;
+//             umap[t[i]]--;
+//         }
         
-        for (auto it: umap) {
-            if (it.second) return false;
-        }
+//         for (auto it: umap) {
+//             if (it.second) return false;
+//         }
         
-        return true;
-    }
-};
+//         return true;
+//     }
+// };
