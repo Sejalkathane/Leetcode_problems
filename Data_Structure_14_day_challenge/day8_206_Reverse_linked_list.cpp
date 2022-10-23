3ef9eecbcbf05839e0a6b0ba0d6437dbb0d113c9
Sejal kathane <kathanesejal@gmail.com>
2022-10-23 23:54:37 +0530

    commit change

--- Data_Structure_14_day_challenge/day8_206_Reverse_linked_list.cpp
+++ Data_Structure_14_day_challenge/day8_206_Reverse_linked_list.cpp
@@ -1,26 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-       ListNode* prev=NULL;
-        ListNode* curr=head;
-        ListNode* next=head;
-        while(next!=NULL){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }
-        head=prev;
-        return head;
-    }
-};
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//        ListNode* prev=NULL;
+//         ListNode* curr=head;
+//         ListNode* next=head;
+//         while(next!=NULL){
+//             next=curr->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=next;
+//         }
+//         head=prev;
+//         return head;
+//     }
+// };
