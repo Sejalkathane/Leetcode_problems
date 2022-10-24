1b78f338252797f52727278f6556d96206500b03
Sejal kathane <kathanesejal@gmail.com>
2022-10-24 17:16:57 +0530

    add problem no 144

--- Data_Structure_14_day_challenge/day9_232_Implement_Queue_using_Stacks.cpp
+++ Data_Structure_14_day_challenge/day9_232_Implement_Queue_using_Stacks.cpp
@@ -1,45 +1,45 @@
-class MyQueue
-{
-public:
-    stack<int> s1;
-    stack<int> s2;
+// class MyQueue
+// {
+// public:
+//     stack<int> s1;
+//     stack<int> s2;
 
-    MyQueue()
-    {
-    }
+//     MyQueue()
+//     {
+//     }
 
-    void push(int x)
-    {
-        while (!s1.empty())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        s2.push(x);
-        while (!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
-    }
+//     void push(int x)
+//     {
+//         while (!s1.empty())
+//         {
+//             s2.push(s1.top());
+//             s1.pop();
+//         }
+//         s2.push(x);
+//         while (!s2.empty())
+//         {
+//             s1.push(s2.top());
+//             s2.pop();
+//         }
+//     }
 
-    int pop()
-    {
-        int curr = s1.top();
-        s1.pop();
-        return curr;
-    }
+//     int pop()
+//     {
+//         int curr = s1.top();
+//         s1.pop();
+//         return curr;
+//     }
 
-    int peek()
-    {
-        return s1.top();
-    }
+//     int peek()
+//     {
+//         return s1.top();
+//     }
 
-    bool empty()
-    {
-        return s1.empty();
-    }
-};
+//     bool empty()
+//     {
+//         return s1.empty();
+//     }
+// };
 
 /**
  * Your MyQueue object will be instantiated and called as such:
