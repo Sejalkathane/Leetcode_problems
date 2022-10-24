2523c0ceb1a0b007225c8815db015facedec8fcf
Sejal kathane <kathanesejal@gmail.com>
2022-10-24 17:18:40 +0530

    add problem no 94

--- Data_Structure_14_day_challenge/day10_144_Binary_Tree_preorder_Traversal.cpp
+++ Data_Structure_14_day_challenge/day10_144_Binary_Tree_preorder_Traversal.cpp
@@ -1,33 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution
-{
-public:
-    void preorder(vector<int> &vec, TreeNode *root)
-    {
-        if (root == NULL)
-        {
-            return;
-        }
-        vec.push_back(root->val);
-        preorder(vec, root->left);
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//     void preorder(vector<int> &vec, TreeNode *root)
+//     {
+//         if (root == NULL)
+//         {
+//             return;
+//         }
+//         vec.push_back(root->val);
+//         preorder(vec, root->left);
 
-        preorder(vec, root->right);
-    }
+//         preorder(vec, root->right);
+//     }
 
-    vector<int> preorderTraversal(TreeNode *root)
-    {
-        vector<int> vec;
-        preorder(vec, root);
-        return vec;
-    }
-};
+//     vector<int> preorderTraversal(TreeNode *root)
+//     {
+//         vector<int> vec;
+//         preorder(vec, root);
+//         return vec;
+//     }
+// };
