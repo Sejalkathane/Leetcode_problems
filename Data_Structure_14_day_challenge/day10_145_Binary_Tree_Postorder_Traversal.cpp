cf4e86bfbe4c544a325ca399fd66689fb5d4745e
Sejal kathane <kathanesejal@gmail.com>
2022-10-24 17:22:23 +0530

    add problem no 102

--- Data_Structure_14_day_challenge/day10_145_Binary_Tree_Postorder_Traversal.cpp
+++ Data_Structure_14_day_challenge/day10_145_Binary_Tree_Postorder_Traversal.cpp
@@ -1,48 +1,48 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution
-{
-public:
-    vector<int> postorderTraversal(TreeNode *root)
-    {
-        if (root == NULL)
-        {
-            return {};
-        }
-        vector<int> ans;
-        stack<TreeNode *> s;
-        s.push(root);
-        while (!s.empty())
-        {
-            TreeNode *curr = s.top();
-            if (curr->left)
-            {
-                s.push(curr->left);
-                curr->left = NULL;
-            }
-            else
-            {
-                if (curr->right)
-                {
-                    s.push(curr->right);
-                    curr->right = NULL;
-                }
-                else
-                {
-                    ans.push_back(curr->val);
-                    s.pop();
-                }
-            }
-        }
-        return ans;
-    }
-};
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//     vector<int> postorderTraversal(TreeNode *root)
+//     {
+//         if (root == NULL)
+//         {
+//             return {};
+//         }
+//         vector<int> ans;
+//         stack<TreeNode *> s;
+//         s.push(root);
+//         while (!s.empty())
+//         {
+//             TreeNode *curr = s.top();
+//             if (curr->left)
+//             {
+//                 s.push(curr->left);
+//                 curr->left = NULL;
+//             }
+//             else
+//             {
+//                 if (curr->right)
+//                 {
+//                     s.push(curr->right);
+//                     curr->right = NULL;
+//                 }
+//                 else
+//                 {
+//                     ans.push_back(curr->val);
+//                     s.pop();
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
---
+++ day11_102_Binary_Tree_Level_order_Traversal.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    //    vector<vector<int>> ret;
+
+    //           void buildVector(TreeNode *root, int depth)
+    //       {
+    //           if(root == NULL)
+    //                return;
+    //           if(ret.size() == depth)
+    //                ret.push_back(vector<int>());
+
+    //           ret[depth].push_back(root->val);
+    //           buildVector(root->left, depth + 1);
+    //           buildVector(root->right, depth + 1);
+    //      }
+
+    //     vector<vector<int> > levelOrder(TreeNode *root)
+    //     {
+    //        buildVector(root, 0);
+    //           return ret;
+    //     }
+
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+        queue<TreeNode *> q;
+        if (root)
+            q.push(root);
+        while (!q.empty())
+        {
+            int len = q.size();
+            vector<int> level;
+            for (int i = 0; i < len; ++i)
+            {
+                TreeNode *node = q.front();
+                level.push_back(node->val);
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+                q.pop();
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
