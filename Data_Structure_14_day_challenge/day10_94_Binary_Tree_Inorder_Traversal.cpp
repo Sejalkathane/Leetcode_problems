7d551a1e06b84b0e1c93b4dba2e0e671c4992807
Sejal kathane <kathanesejal@gmail.com>
2022-10-24 17:20:05 +0530

    add problem no 145

--- Data_Structure_14_day_challenge/day10_94_Binary_Tree_Inorder_Traversal.cpp
+++ Data_Structure_14_day_challenge/day10_94_Binary_Tree_Inorder_Traversal.cpp
@@ -1,32 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution
-{
-public:
-    void inorder(vector<int> &vec, TreeNode *root)
-    {
-        if (root == NULL)
-        {
-            return;
-        }
-        inorder(vec, root->left);
-        vec.push_back(root->val);
-        inorder(vec, root->right);
-    }
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//     void inorder(vector<int> &vec, TreeNode *root)
+//     {
+//         if (root == NULL)
+//         {
+//             return;
+//         }
+//         inorder(vec, root->left);
+//         vec.push_back(root->val);
+//         inorder(vec, root->right);
+//     }
 
-    vector<int> inorderTraversal(TreeNode *root)
-    {
-        vector<int> vec;
-        inorder(vec, root);
-        return vec;
-    }
-};
+//     vector<int> inorderTraversal(TreeNode *root)
+//     {
+//         vector<int> vec;
+//         inorder(vec, root);
+//         return vec;
+//     }
+// };
